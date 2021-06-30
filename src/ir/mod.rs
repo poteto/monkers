3e8ddf9a0814f169db8553b028b1ef187ffb64ca
Lauren Tan <poteto@users.noreply.github.com>
2021-06-29 21:23:04 -0400

    Add tests

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,10 +1,12 @@
-mod integer;
 mod boolean;
+mod integer;
 mod null;
 
-pub use integer::IRInteger;
 pub use boolean::IRBoolean;
+pub use integer::IRInteger;
 pub use null::IRNull;
+
+use std::fmt;
 
 trait IRTypedObject {
     const TYPE: IRObjectKind;
@@ -20,5 +22,16 @@ pub enum IR {
     Integer(IRInteger),
     Boolean(IRBoolean),
     Null(IRNull),
-    NotImplemented
+    NotImplementedYet,
+}
+
+impl fmt::Display for IR {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            IR::Integer(ir) => ir.fmt(f),
+            IR::Boolean(ir) => ir.fmt(f),
+            IR::Null(ir) => ir.fmt(f),
+            IR::NotImplementedYet => write!(f, "Not Implemented"),
+        }
+    }
 }
